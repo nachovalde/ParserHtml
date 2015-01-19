9ffc6c45b319309d6da90ee014abb24fe57977d7
nachovalde <ivalderrama.lorca@gmail.com>
2015-01-19 17:32:44 -0300

    obtencion de nombres del informe.
    a partir de la url se obtiene rut, mes y año

--- parser.py
+++ parser.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 import urllib2
 import xlsxwriter
 from pyparsing import *
+import re
 
 tdStart,tdEnd = makeHTMLTags("td")
 td = tdStart + SkipTo(tdEnd).setResultsName("data") + tdEnd
-def procesar(table, cantidad):
-        #file=open("tabla" + str(cantidad) + ".xlsx","w")
-        workbook=xlsxwriter.Workbook("libro"+str(cantidad)+".xlsx")
+def procesar(table, nombre):
+        workbook=xlsxwriter.Workbook(nombre+".xlsx")
         worksheet=workbook.add_worksheet()
         worksheet.write(0,0,"nombre")
         worksheet.write(0,1,"valor")
@@ -36,17 +36,22 @@ def procesar(table, cantidad):
         workbook.close()
 
 url="http://www.svs.cl/institucional/mercados/entidad.php?auth=&send=&mercado=V&rut=61808000&rut_inc=&grupo=0&tipoentidad=RVEMI&vig=VI&row=AABbBQABwAAAA5TAAm&mm=12&aa=2013&tipo=C&orig=lista&control=svs&tipo_norma=IFRS&pestania=3"
+#Obtencion de datos del archivo(anio, mes y rut operador)
+ini = url.find("aa=")+3
+anio = url[ini:ini+4]
+ini = url.find("mm=")+3
+mes = url[ini:ini+2]
+ini = url.find("rut=")+4
+rut = url[ini:ini+8]
+nombre_archivo = rut+"_"+mes+"_"+anio
+print nombre_archivo
+
 page = urllib2.urlopen(url)
 soup=BeautifulSoup(page)
 soup_tables=soup.findAll("table",limit=4)
 c=0
 for table in soup_tables:
 	c+=1
-	procesar(table, c)
+	procesar(table, nombre_archivo)
 	if(c==2):
 		break
-'''
-file=open("html.txt","w")
-file.write(str(soup_tables))
-file.close()
-'''
