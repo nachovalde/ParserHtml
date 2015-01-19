c441b8dc98da5a0d54660a3f680629ab17e92ed1
raticate <caterina.munoz.v@gmail.com>
2015-01-19 15:57:16 -0300

    arreglín en el excel

--- parser-cate.py
+++ parser-cate.py
@@ -19,21 +19,14 @@ def procesar(table, cantidad):
         coln=0
         for row in rows:
         	cols=row.findAll("td")
-        	#row=[]
         	for col in cols:
-        		#print col.text
         		for tokens,start,end in td.scanString(str(col)):
         			row.append(tokens.data.strip())
-        			#file.write(tokens.data.strip()+"\n")
         			worksheet.write(rown,coln,tokens.data.encode('utf-8').strip())
         		coln+=1
         	rown+=1
+        	coln=0
         workbook.close()
-        	#file.write(";".join(row)+"\n")
-        #for row in table:
-        	#file.write(" ".join(row))
-        #file.close()
-
 
 url="http://www.svs.cl/institucional/mercados/entidad.php?auth=&send=&mercado=V&rut=61808000&rut_inc=&grupo=0&tipoentidad=RVEMI&vig=VI&row=AABbBQABwAAAA5TAAm&mm=12&aa=2013&tipo=C&orig=lista&control=svs&tipo_norma=IFRS&pestania=3"
 page = urllib2.urlopen(url)
