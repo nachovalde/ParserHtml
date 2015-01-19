f46c68afd1d09a3ea3263473246ee718ad7d7c53
nachovalde <ivalderrama.lorca@gmail.com>
2015-01-19 14:42:23 -0300

    Agregar funcion procesos

--- parser.py
+++ parser.py
@@ -1,19 +1,21 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-
+def procesar(row, cantidad):
+        file=open("informe" + str(cantidad) + ".txt","w")
+        col=row.findAll("td")
+        file.write(str(col))
+        file.close()
 url="http://www.svs.cl/institucional/mercados/entidad.php?auth=&send=&mercado=V&rut=61808000&rut_inc=&grupo=0&tipoentidad=RVEMI&vig=VI&row=AABbBQABwAAAA5TAAm&mm=12&aa=2013&tipo=C&orig=lista&control=svs&tipo_norma=IFRS&pestania=3"
 page = urllib2.urlopen(url)
 soup=BeautifulSoup(page)
 soup_tables=soup.findAll("table",limit=4)
-file2=open("html2.txt","w")
 c=0
 for row in soup_tables:
 	c+=1
-	col=row.findAll("td")
-	file2.write(str(col))
-print c
+	procesar(row, c)
+'''
 file=open("html.txt","w")
 file.write(str(soup_tables))
 file.close()
-file2.close()
+'''
