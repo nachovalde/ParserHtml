d5353fd4ec8d2f93c49b259aad540634e8560166
nachovalde <ivalderrama.lorca@gmail.com>
2015-01-20 12:54:44 -0300

    Scripts para todos los operadores

--- bash.py
+++ bash.py
@@ -14,7 +14,7 @@ rut_op = [61808000,99542570,99540870,
 76000739,
 96579800]
 
-for rut in rut_op[1:2]:
+for rut in rut_op:
   for x in  range(6):
     anio = x + 2008
     os.system("parser.py 12 " + str(anio)+" "+str(rut))
